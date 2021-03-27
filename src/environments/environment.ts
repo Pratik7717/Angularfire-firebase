2a140ec0d79adbaceb4f09136588747730a86d80
Pratik Jagade <pratik.21810767@viit.ac.in>
2021-03-27 17:27:40 +0530

    angular fire setup done.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDEfXsApoSv3tEqAn-qF5FiTN3jb4TpP2Y",
+    authDomain: "cloud-firebase-assignment.firebaseapp.com",
+    databaseURL: "https://cloud-firebase-assignment-default-rtdb.firebaseio.com",
+    projectId: "cloud-firebase-assignment",
+    storageBucket: "cloud-firebase-assignment.appspot.com",
+    messagingSenderId: "533919540001",
+    appId: "1:533919540001:web:0edf9806e3fbc7ab321460",
+    measurementId: "G-JR158ZTKM7"
+  }
 };
 
 /*
